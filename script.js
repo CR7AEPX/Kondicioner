5304130ba0a1a1da9f15f51910138636e759d15d
CR7AEPX <a.vorisov.09@gmail.com>
2025-04-11 00:03:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,78 @@
-let totalSeconds = 6 * 3600 + 14 * 60 + 49;
-function updateCountdown() {
-  if (totalSeconds <= 0) {
-    document.getElementById("hours").textContent = "00";
-    document.getElementById("minutes").textContent = "00";
-    document.getElementById("seconds").textContent = "00";
-    return;
+  let totalSeconds = 6 * 3600 + 14 * 60 + 49;
+  function updateCountdown() {
+    if (totalSeconds <= 0) {
+      document.getElementById("hours").textContent = "00";
+      document.getElementById("minutes").textContent = "00";
+      document.getElementById("seconds").textContent = "00";
+      return;
+    }
+
+    let hours = Math.floor(totalSeconds / 3600);
+    let minutes = Math.floor((totalSeconds % 3600) / 60);
+    let seconds = totalSeconds % 60;
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+    totalSeconds--;
+  }
+  updateCountdown();
+  setInterval(updateCountdown, 1000);
+  const sliderTrack = document.querySelector(".slider-track");
+  if (sliderTrack) {
+    const slides = sliderTrack.querySelectorAll("img");
+    let currentIndex = 0;
+    const totalSlides = slides.length;
+
+    function updateSlider() {
+      sliderTrack.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    function nextSlide() {
+      currentIndex = (currentIndex + 1) % totalSlides;
+      updateSlider();
+    }
+    setInterval(nextSlide, 3000);
   }
 
-  let hours = Math.floor(totalSeconds / 3600);
-  let minutes = Math.floor((totalSeconds % 3600) / 60);
-  let seconds = totalSeconds % 60;
+  (function (){
+    var myReviewsInit = function () {
+      new window.myReviews.BlockWidget({
+      uuid: "af30b6c1-fac8-4782-b61a-31c8c1a3292e",
+      name: "g64048573",
+      additionalFrame:"none",
+      lang:"ru",
+      widgetId: "0"
+      }).init();
 
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-
-  document.getElementById("hours").textContent = hours;
-  document.getElementById("minutes").textContent = minutes;
-  document.getElementById("seconds").textContent = seconds;
-
-  totalSeconds--;
-}
-updateCountdown();
-setInterval(updateCountdown, 1000);
-const sliderTrack = document.querySelector(".slider-track");
-if (sliderTrack) {
-  const slides = sliderTrack.querySelectorAll("img");
-  let currentIndex = 0;
-  const totalSlides = slides.length;
-
-  function updateSlider() {
-    sliderTrack.style.transform = `translateX(-${currentIndex * 100}%)`;
+    };
+  if (document.readyState === "loading") {
+    document.addEventListener('DOMContentLoaded', function () {
+        myReviewsInit()
+    })
+  } else {
+    myReviewsInit()
   }
+  })()
+  const faqItems = document.querySelectorAll('.faq-item');
+  faqItems.forEach(item => {
+    const answer = item.querySelector('.faq-answer');
 
-  function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalSlides;
-    updateSlider();
-  }
-  setInterval(nextSlide, 3000);
-}
+    item.addEventListener('click', () => {
+      const isOpen = item.classList.contains('active');
+
+      faqItems.forEach(el => {
+        el.classList.remove('active');
+        el.querySelector('.faq-answer').style.maxHeight = null;
+      });
+      if (!isOpen) {
+        item.classList.add('active');
+        answer.style.maxHeight = answer.scrollHeight + 'px';
+      }
+    });
+  });
